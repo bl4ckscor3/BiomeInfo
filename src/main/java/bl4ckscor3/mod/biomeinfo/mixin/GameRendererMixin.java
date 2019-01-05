8b716cd08b15261b5fadb9f6f6e32e425312f6dc
bl4ckscor3 <donalddani9612@gmx.de>
2019-01-05 03:18:29 +0100

    fabric port
    configuration is missing

---
+++ src/main/java/bl4ckscor3/mod/biomeinfo/mixin/GameRendererMixin.java
@@ -0,0 +1,49 @@
+package bl4ckscor3.mod.biomeinfo.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.WorldChunk;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin
+{
+	@Inject(
+			at = @At(
+					value = "INVOKE",
+					target = "Lcom/mojang/blaze3d/platform/GlStateManager;alphaFunc(IF)V"
+					),
+			method = "method_3192")
+	public void method_3192(float partialTicks, long nanoTime, boolean draw, CallbackInfo info)
+	{
+		if(!MinecraftClient.getInstance().options.debugEnabled)
+		{
+			MinecraftClient mc = MinecraftClient.getInstance();
+
+			if(mc.world != null)
+			{
+				BlockPos pos = new BlockPos(mc.getCameraEntity().x, mc.getCameraEntity().getBoundingBox().minY, mc.getCameraEntity().z);
+
+				if(mc.world.isBlockLoaded(pos) && pos.getY() >= 0 && pos.getY() < 256)
+				{
+					WorldChunk chunk = mc.world.getChunk(pos);
+
+					if(!chunk.method_12223())
+					{
+						GlStateManager.pushMatrix();
+						GlStateManager.scaled(2.0D, 2.0D, 2.0D);
+						mc.fontRenderer.drawWithShadow(chunk.getBiome(pos).getTextComponent().getFormattedText(), 10, 10, 0xffffff);
+						GlStateManager.popMatrix();
+					}
+				}
+			}
+		}
+	}
+}
