278b82da868f84fbfe569689f59460ba51fe3d0c
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-12-19 13:50:35 +0100

    update to 1.15.1

--- src/main/java/bl4ckscor3/mod/biomeinfo/mixin/GameRendererMixin.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/mixin/GameRendererMixin.java
@@ -5,13 +5,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.biome.Biome;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin
@@ -19,7 +18,7 @@ public class GameRendererMixin
 	@Inject(
 			at = @At(
 					value = "INVOKE",
-					target = "Lcom/mojang/blaze3d/platform/GlStateManager;alphaFunc(IF)V"
+					target = "Lnet/minecraft/client/gui/hud/InGameHud;render(F)V"
 					),
 			method = "render")
 	public void render(float partialTicks, long nanoTime, boolean draw, CallbackInfo info)
@@ -30,18 +29,18 @@ public class GameRendererMixin
 
 			if(mc.world != null)
 			{
-				BlockPos pos = new BlockPos(mc.getCameraEntity().x, mc.getCameraEntity().getBoundingBox().minY, mc.getCameraEntity().z);
+				BlockPos pos = new BlockPos(mc.getCameraEntity());
 
-				if(mc.world.isBlockLoaded(pos) && pos.getY() >= 0 && pos.getY() < 256)
+				if(pos.getY() >= 0 && pos.getY() < 256)
 				{
-					Chunk chunk = mc.world.getChunk(pos);
+					Biome biome = mc.world.getBiome(pos);
 
-					if(chunk instanceof WorldChunk && !((WorldChunk)chunk).isEmpty())
+					if(biome != null)
 					{
-						GlStateManager.pushMatrix();
-						GlStateManager.scaled(2.0D, 2.0D, 2.0D);
-						mc.textRenderer.drawWithShadow(chunk.getBiome(pos).getName().asFormattedString(), 5, 5, 0xffffff);
-						GlStateManager.popMatrix();
+						RenderSystem.pushMatrix();
+						RenderSystem.scaled(1.0D, 1.0D, 1.0D);
+						mc.textRenderer.drawWithShadow(biome.getName().asFormattedString(), 5, 5, 0xffffff);
+						RenderSystem.popMatrix();
 					}
 				}
 			}
