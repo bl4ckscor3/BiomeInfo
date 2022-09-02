73639facb7c0f7f559099c0bc772b182387f786e
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-09-02 13:40:14 +0200

    add config setting for text alignment

---
+++ src/main/java/bl4ckscor3/mod/biomeinfo/TextAlignment.java
@@ -0,0 +1,25 @@
+package bl4ckscor3.mod.biomeinfo;
+
+import java.util.function.BiFunction;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.network.chat.Component;
+
+public enum TextAlignment
+{
+	LEFT((font, component) -> 0),
+	MIDDLE((font, component) -> font.width(component) / 2),
+	RIGHT((font, component) -> font.width(component));
+
+	private final BiFunction<Font,Component,Integer> negativeOffset;
+
+	TextAlignment(BiFunction<Font,Component,Integer> offset)
+	{
+		this.negativeOffset = offset;
+	}
+
+	public int getNegativeOffset(Font font, Component biomeName)
+	{
+		return negativeOffset.apply(font, biomeName);
+	}
+}
