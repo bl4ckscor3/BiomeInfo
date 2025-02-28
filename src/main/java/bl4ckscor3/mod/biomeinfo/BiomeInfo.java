e17b5ebccccf8d9bc5ac39df5bdf67d2c2532be6
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-02-28 21:06:38 +0100

    Add universal biome name formatting and biome name cache

--- src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
@@ -1,5 +1,12 @@
 package bl4ckscor3.mod.biomeinfo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import org.apache.commons.lang3.StringUtils;
+
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
 
@@ -8,20 +15,28 @@ import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.biome.Biome;
 
-public class BiomeInfo implements ClientModInitializer {
+public class BiomeInfo implements ClientModInitializer, IdentifiableResourceReloadListener {
 	public static final int MARGIN = 3;
 	static BiomeInfoConfig config;
 	private Biome previousBiome;
 	private int displayTime = 0;
 	private int alpha = 0;
 	private boolean fadingIn = false;
+	public static final Map<ResourceKey<Biome>, Component> NAME_CACHE = new HashMap<>();
 
 	@Override
 	public void onInitializeClient() {
@@ -77,8 +92,8 @@ public class BiomeInfo implements ClientModInitializer {
 
 					if (alpha > 0) {
 						biomeHolder.unwrapKey().ifPresent(key -> {
+							Component biomeName = getBiomeName(key);
 							float scale = (float) config.scale;
-							Component biomeName = Component.translatable(Util.makeDescriptionId("biome", key.location()));
 							PositionPreset positionPreset = config.positionPreset;
 							int textOffset = positionPreset.textAlignment().getNegativeOffset(mc.font, biomeName);
 							PoseStack pose = graphics.pose();
@@ -93,5 +108,44 @@ public class BiomeInfo implements ClientModInitializer {
 				}
 			}
 		});
+		ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(this);
+	}
+
+	private static Component getBiomeName(ResourceKey<Biome> key) {
+		return NAME_CACHE.computeIfAbsent(key, k -> {
+			String translationKey = Util.makeDescriptionId("biome", key.location());
+			Component biomeName = Component.translatable(translationKey);
+			String displayedText = biomeName.getString();
+
+			if (displayedText.equals(translationKey)) {
+				String biomePath = key.location().getPath(); //e.g. "birch_forest"
+				String formattedBiomeName = formatBiomeName(biomePath);
+
+				return Component.literal(formattedBiomeName);
+			}
+
+			return biomeName;
+		});
+	}
+
+	private static String formatBiomeName(String biomePath) {
+		String[] words = biomePath.split("_");
+		StringBuilder formatted = new StringBuilder();
+
+		for (String word : words) {
+			formatted.append(StringUtils.capitalize(word)).append(" ");
+		}
+
+		return formatted.toString().trim();
+	}
+
+	@Override
+	public CompletableFuture<Void> reload(PreparationBarrier preparationBarrier, ResourceManager resourceManager, ProfilerFiller profilerFiller, ProfilerFiller profilerFiller2, Executor executor, Executor executor2) {
+		return CompletableFuture.runAsync(NAME_CACHE::clear, executor).thenCompose(preparationBarrier::wait);
+	}
+
+	@Override
+	public ResourceLocation getFabricId() {
+		return ResourceLocation.fromNamespaceAndPath("biomeinfo", "cache_invalidation");
 	}
 }
