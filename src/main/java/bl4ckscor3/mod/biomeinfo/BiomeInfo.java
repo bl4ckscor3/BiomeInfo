ade1bd1734a29fde1e5239dda4f8f73f447d98d0
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-03-31 20:34:00 +0200

    Fix changing config not invalidating cache

--- src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
@@ -11,6 +11,7 @@ import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -45,7 +46,12 @@ public class BiomeInfo implements ClientModInitializer, IdentifiableResourceRelo
 	@Override
 	public void onInitializeClient() {
 		AutoConfig.register(BiomeInfoConfig.class, JanksonConfigSerializer::new);
-		config = AutoConfig.getConfigHolder(BiomeInfoConfig.class).getConfig();
+		ConfigHolder<BiomeInfoConfig> configHolder = AutoConfig.getConfigHolder(BiomeInfoConfig.class);
+		configHolder.registerSaveListener((holder, config) -> {
+			NAME_CACHE.clear();
+			return InteractionResult.SUCCESS;
+		});
+		config = configHolder.getConfig();
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			if (!fadingIn) {
 				if (!config.fadeOut && alpha != 255)
