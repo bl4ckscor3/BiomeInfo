e8e3e31442c3aeee694158d51791da2a735f4478
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-01-17 10:49:31 +0100

    Formatting

--- src/main/java/bl4ckscor3/mod/biomeinfo/PositionPreset.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/PositionPreset.java
@@ -11,27 +11,95 @@ import net.minecraft.client.gui.Font;
 import static bl4ckscor3.mod.biomeinfo.BiomeInfoRenderer.MARGIN;
 
 public enum PositionPreset {
-	NONE(window -> Configuration.posX(), (window, font) -> Configuration.posY(), Configuration::textAlignment),
-	TOP_LEFT(window -> MARGIN, (window, font) -> MARGIN, () -> TextAlignment.LEFT),
-	TOP_MIDDLE(window -> window.getGuiScaledWidth() / 2, (window, font) -> MARGIN, () -> TextAlignment.MIDDLE),
-	TOP_RIGHT(window -> window.getGuiScaledWidth() - MARGIN, (window, font) -> MARGIN, () -> TextAlignment.RIGHT),
-	MIDDLE_LEFT(window -> MARGIN, (window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2, () -> TextAlignment.LEFT),
-	MIDDLE(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2, () -> TextAlignment.MIDDLE),
-	MIDDLE_RIGHT(window -> window.getGuiScaledWidth() - MARGIN, (window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2, () -> TextAlignment.RIGHT),
-	BOTTOM_LEFT(window -> MARGIN, (window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight, () -> TextAlignment.LEFT),
-	BOTTOM_MIDDLE(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight, () -> TextAlignment.MIDDLE),
-	BOTTOM_RIGHT(window -> window.getGuiScaledWidth() - MARGIN, (window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight, () -> TextAlignment.RIGHT),
-	ABOVE_MIDDLE(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() / 4, () -> TextAlignment.MIDDLE),
-	ABOVE_HOTBAR(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() - 68, () -> TextAlignment.MIDDLE),
-	LEFT_OF_CROSSHAIR(window -> window.getGuiScaledWidth() / 2 - MARGIN - 3, (window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2, () -> TextAlignment.RIGHT),
-	RIGHT_OF_CROSSHAIR(window -> window.getGuiScaledWidth() / 2 + MARGIN + 3, (window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2, () -> TextAlignment.LEFT),
-	ABOVE_CROSSHAIR(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() / 2 - MARGIN - 3 - font.lineHeight, () -> TextAlignment.MIDDLE),
-	UNDER_CROSSHAIR_WITH_ATTACK_INDICATOR(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() / 2 + MARGIN + 4 + font.lineHeight, () -> TextAlignment.MIDDLE),
-	UNDER_CROSSHAIR(window -> window.getGuiScaledWidth() / 2, (window, font) -> window.getGuiScaledHeight() / 2 + MARGIN + 3, () -> TextAlignment.MIDDLE);
+	NONE(
+		window -> Configuration.posX(),
+		(window, font) -> Configuration.posY(),
+		Configuration::textAlignment
+	),
+	TOP_LEFT(
+		window -> MARGIN,
+		(window, font) -> MARGIN,
+		() -> TextAlignment.LEFT
+	),
+	TOP_MIDDLE(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> MARGIN,
+		() -> TextAlignment.MIDDLE
+	),
+	TOP_RIGHT(
+		window -> window.getGuiScaledWidth() - MARGIN,
+		(window, font) -> MARGIN,
+		() -> TextAlignment.RIGHT
+	),
+	MIDDLE_LEFT(
+		window -> MARGIN,
+		(window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2,
+		() -> TextAlignment.LEFT
+	),
+	MIDDLE(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2,
+		() -> TextAlignment.MIDDLE
+	),
+	MIDDLE_RIGHT(
+		window -> window.getGuiScaledWidth() - MARGIN,
+		(window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2,
+		() -> TextAlignment.RIGHT
+	),
+	BOTTOM_LEFT(
+		window -> MARGIN,
+		(window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight,
+		() -> TextAlignment.LEFT
+	),
+	BOTTOM_MIDDLE(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight,
+		() -> TextAlignment.MIDDLE
+	),
+	BOTTOM_RIGHT(
+		window -> {return window.getGuiScaledWidth() - MARGIN;},
+		(window, font) -> window.getGuiScaledHeight() - MARGIN - font.lineHeight,
+		() -> TextAlignment.RIGHT
+	),
+	ABOVE_MIDDLE(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() / 4,
+		() -> TextAlignment.MIDDLE
+	),
+	ABOVE_HOTBAR(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() - 68,
+		() -> TextAlignment.MIDDLE
+	),
+	LEFT_OF_CROSSHAIR(
+		window -> window.getGuiScaledWidth() / 2 - MARGIN - 3,
+		(window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2,
+		() -> TextAlignment.RIGHT
+	),
+	RIGHT_OF_CROSSHAIR(
+		window -> window.getGuiScaledWidth() / 2 + MARGIN + 3,
+		(window, font) -> window.getGuiScaledHeight() / 2 - font.lineHeight / 2,
+		() -> TextAlignment.LEFT
+	),
+	ABOVE_CROSSHAIR(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() / 2 - MARGIN - 3 - font.lineHeight,
+		() -> TextAlignment.MIDDLE
+	),
+	UNDER_CROSSHAIR_WITH_ATTACK_INDICATOR(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() / 2 + MARGIN + 4 + font.lineHeight,
+		() -> TextAlignment.MIDDLE
+	),
+	UNDER_CROSSHAIR(
+		window -> window.getGuiScaledWidth() / 2,
+		(window, font) -> window.getGuiScaledHeight() / 2 + MARGIN + 3,
+		() -> TextAlignment.MIDDLE
+	);
 
-	private ToIntFunction<Window> xGetter;
-	private ToIntBiFunction<Window, Font> yGetter;
-	private Supplier<TextAlignment> textAlignmentGetter;
+	private final ToIntFunction<Window> xGetter;
+	private final ToIntBiFunction<Window, Font> yGetter;
+	private final Supplier<TextAlignment> textAlignmentGetter;
 
 	PositionPreset(ToIntFunction<Window> xGetter, ToIntBiFunction<Window, Font> yGetter, Supplier<TextAlignment> textAlignmentGetter) {
 		this.xGetter = xGetter;
