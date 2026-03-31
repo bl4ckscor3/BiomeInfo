9e78e637d03af41533a79ed2a7e5b0347d0984c1
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2026-03-30 21:51:47 -0400

    Port to 26.1

--- src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfoRenderer.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfoRenderer.java
@@ -11,7 +11,7 @@ import com.mojang.blaze3d.platform.Window;
 
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
@@ -73,7 +73,7 @@ public class BiomeInfoRenderer {
 		}
 	}
 
-	public static void renderBiomeInfo(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+	public static void renderBiomeInfo(GuiGraphicsExtractor guiGraphics, DeltaTracker deltaTracker) {
 		if (complete && Configuration.enabled()) {
 			Minecraft mc = Minecraft.getInstance();
 
@@ -115,7 +115,7 @@ public class BiomeInfoRenderer {
 
 						pose.pushMatrix();
 						pose.scale(scale, scale);
-						guiGraphics.drawString(
+						guiGraphics.text(
 							mc.font,
 							biomeName,
 							(int) ((positionPreset.posX(window) - textOffset) / scale),
